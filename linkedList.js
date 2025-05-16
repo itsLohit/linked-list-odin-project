efb471d1139eea94f36a7005526f5ae3eb4994c5
itsLohit <109834876+itsLohit@users.noreply.github.com>
2025-05-16 09:57:07 +0530

    Completed remaining functions.

--- linkedList.js
+++ linkedList.js
@@ -57,6 +57,67 @@ class LinkedList {
             return this.at(num - 1);
         }
     }
+
+    at(index){
+        let current = this.head;
+        for(let i = 0; current !== null && i < index; i++) {
+            current = current.nextNode;
+        }
+        return current;
+    }
+
+    pop(){
+        let num = this.size();
+        if(num === 0) {
+            return;
+        }
+        if(num === 1) {
+            this.head = null;
+        }
+        else {
+            let current = this.head;
+            for(let i = 0; i < num - 2; i++) {
+                current = current.nextNode;
+            }
+            current.nextNode = null;
+        }
+    }
+
+    contains(value){
+        let current = this.head;
+        while (current !== null) {
+            if(value === current.value) {
+                return true;
+            }
+            current = current.nextNode;
+        }
+        return false;
+    }
+
+    find(value){
+        let current = this.head;
+        let counter = 0;
+        while (current !== null) {
+            if(value === current.value) {
+                return counter;
+            }
+            current = current.nextNode;
+            counter++;
+        }
+        return null;
+    }
+
+    toString(){
+        let finalString = "";
+        let current = this.head;
+
+        while (current !== null) {
+            finalString += `( ${current.value} ) -> `;
+            current = current.nextNode;
+        }
+        finalString += "null";
+        return finalString;
+    }
 }
 
 class Node{
